5165577083cd8281de2a60b0cc734e284df8e830
mtmsujan <mdraju3248@gmail.com>
2025-05-09 19:34:05 +0600

    Added header react project

---
+++ src/routes/routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayouts from "../layouts/HomeLayouts";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: HomeLayouts,
+  },
+]);
+
+export default router;
