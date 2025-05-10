3472b7486fd90013970e52b8c91e65138d2c1aa8
mtmsujan <mdraju3248@gmail.com>
2025-05-11 00:52:29 +0600

    change useContext react hook

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,28 @@
-// import React, { useContext } from 'react'
+import { use, useContext } from 'react';
 import { Link, NavLink } from 'react-router'
 import userIcon from "../assets/user.png";
 import { authContext } from '../Provider/AuthProvider';
 
 const Navbar = () => {
 
-  // const {user} = useContext(authContext);
+  const {user} = use(authContext);
 
   return (
-    <div className='flex justify-between items-center'>
-        <div></div>
-        <div className='flex gap-5 text-accent'>
-            <NavLink to={'/'}>Home</NavLink>
-            <NavLink to={'/about'}>About</NavLink>
-            <NavLink to={'/career'}>Career</NavLink>
-        </div>
-        <div className='flex items-center gap-5'>
-            <img src={userIcon} alt="" />
-            <Link to={'/auth/login'} className='btn btn-primary px-7'>Login</Link>
-        </div>
+    <div className="flex justify-between items-center">
+      <div>{user?.email}</div>
+      <div className="flex gap-5 text-accent">
+        <NavLink to={"/"}>Home</NavLink>
+        <NavLink to={"/about"}>About</NavLink>
+        <NavLink to={"/career"}>Career</NavLink>
+      </div>
+      <div className="flex items-center gap-5">
+        <img src={userIcon} alt="" />
+        <Link to={"/auth/login"} className="btn btn-primary px-7">
+          Login
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Navbar
