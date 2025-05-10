cc23f87a125ff184b740813ea798d86c1afa8ed7
mtmsujan <mdraju3248@gmail.com>
2025-05-10 19:22:08 +0600

    Sticky implement to aside home layouts

--- src/layouts/HomeLayouts.jsx
+++ src/layouts/HomeLayouts.jsx
@@ -12,21 +12,21 @@ const HomeLayouts = () => {
       <header>
         <Header></Header>
         <section className="w-11/12 mx-auto">
-            <LatesNews></LatesNews>
+          <LatesNews></LatesNews>
         </section>
         <nav className="w-11/12 mx-auto">
-            <Navbar></Navbar>
+          <Navbar></Navbar>
         </nav>
       </header>
       <main className="w-11/12 mx-auto my-8 grid grid-cols-12 gap-5">
-        <aside className="col-span-3">
-            <LeftAside></LeftAside>
+        <aside className="col-span-3 sticky top-0 h-fit">
+          <LeftAside></LeftAside>
         </aside>
         <section className="main col-span-6">
-            <Outlet></Outlet>
+          <Outlet></Outlet>
         </section>
-        <aside className="col-span-3">
-            <RightAside></RightAside>
+        <aside className="col-span-3 sticky top-0 h-fit">
+          <RightAside></RightAside>
         </aside>
       </main>
     </div>
